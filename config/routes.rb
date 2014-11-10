104da9627d5d947a2a109c09e2a68416acd6302c
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2014-11-10 13:51:31 -0700

    set up static pages and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
+
   resources :personnels
 
   # The priority is based upon order of creation: first created -> highest priority.
