6f4679b55f5dd16175c3b24d1440f236c1d7b1dd
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2014-12-01 20:17:32 -0700

    Started Nav menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
 
   get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/visit', to: 'static_pages#visit'
 
   resources :personnels
 
